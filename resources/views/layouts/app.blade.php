e5d02d8a9312855dfb1442c97bd92408e667160e
Jean Mateus <gammawrecked00@gmail.com>
2025-12-01 11:50:26 -0300

    Pequena correção visual mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -403,12 +403,6 @@
                 <a href="{{ route('register') }}" class="btn-signup">Cadastro</a>
                 @endauth
             </div>
-
-            <button class="mobile-menu-button" aria-label="Menu">
-                <svg class="mobile-menu-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
-            </button>
         </div>
     </header>
 
